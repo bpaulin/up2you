bbfad3ae0f56ba4042c186191c5ed5c1573fdcb0
Bruno Paulin <brunopaulin@bpaulin.net>
2016-02-13 09:14:51 +0100

    get votes yes/no/maybe

--- src/AppBundle/Controller/VotesController.php
+++ src/AppBundle/Controller/VotesController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Vote;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 
@@ -20,5 +21,42 @@ class VotesController extends FOSRestController
         $view = $this->view($data, 200);
 
         return $this->handleView($view);
+    }
+
+    protected function getVote($vote)
+    {
+        $data = $this->getDoctrine()->getRepository('AppBundle:Vote')->findBy(
+            array(
+                'user' => $this->getUser(),
+                'vote' => $vote
+            )
+        );
+        $view = $this->view($data, 200);
+
+        return $this->handleView($view);
+    }
+
+    /**
+     * @REST\Get("votes/yes")
+     */
+    public function getVotesYesAction()
+    {
+        return $this->getVote(Vote::YES);
+    }
+
+    /**
+     * @REST\Get("votes/no")
+     */
+    public function getVotesNoAction()
+    {
+        return $this->getVote(Vote::NO);
+    }
+
+    /**
+     * @REST\Get("votes/maybe")
+     */
+    public function getVotesMaybeAction()
+    {
+        return $this->getVote(Vote::MAYBE);
     }
 }
--- tests/AppBundle/Controller/VotesControllerTest.php
+++ tests/AppBundle/Controller/VotesControllerTest.php
@@ -27,4 +27,46 @@ class VotesControllerTest extends BaseTestCase
             array_keys($data[0]["proposal"])
         );
     }
+
+    public function votersCanViewVotesProvider()
+    {
+        return array(
+            array('/api/votes/maybe', 0),
+            array('/api/votes/yes', 1),
+            array('/api/votes/no', -1),
+        );
+    }
+
+    /**
+     * @dataProvider votersCanViewVotesProvider
+     */
+    public function testVotersCanViewVotesMaybe($path, $value)
+    {
+        $this->loadFixtureFiles(array(
+            '@AppBundle/DataFixtures/ORM/test.yml'
+        ));
+        $client = $this->createClientAuthenticatedAs('voter1');
+
+        $client->request('GET', $path);
+
+        $this->isSuccessful($client->getResponse());
+        $this->assertJson($client->getResponse()->getContent());
+
+        $data = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertEquals(
+            array("id", "vote", "proposal"),
+            array_keys($data[0])
+        );
+        $this->assertEquals(
+            array("id", "name"),
+            array_keys($data[0]["proposal"])
+        );
+        foreach ($data as $vote) {
+            $this->assertEquals(
+                $value,
+                $vote["vote"]
+            );
+        }
+    }
 }
